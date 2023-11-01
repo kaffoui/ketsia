fb9590b3cd5a289b0dd4248407bfc9a88407cef7
TOZO Kétsia <ktozo@armag.local>
2023-11-01 19:19:16 +0100

    Inscription et connexion

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+
+        public function __construct()
+        {
+            $this->middleware('guest')->except([
+                'logout', 'dashboard'
+            ]);
+        }
+
+
+
+        public function register() {
+            return view('auth.register');
+        }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:250',
+            'email' => 'required|email|max:250|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        Auth::attempt($credentials);
+        $request->session()->regenerate();
+        return redirect()->route('dashboard')
+        ->withSuccess('Vous êtes inscrit et connecté avec succès!');
+    }
+
+        public function login() {
+            return view('auth.login');
+        }
+
+    
+        public function authenticate(Request $request)
+        {
+            $credentials = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+    
+            if(Auth::attempt($credentials))
+            {
+                $request->session()->regenerate();
+                return redirect()->route('dashboard')
+                    ->withSuccess('Vous êtes connecté avec succès!');
+            }
+    
+            return back()->withErrors([
+                'email' => 'Les informations que vous avez fournies ne sont pas correctes.',
+            ])->onlyInput('email');
+    
+        } 
+
+
+        public function dashboard()
+    {
+        if(Auth::check())
+        {
+            return view('auth.dashboard');
+        }
+        
+        return redirect()->route('login')
+            ->withErrors([
+            'email' => 'Veuillez vous connecter !!.',
+        ])->onlyInput('email');
+    } 
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')
+            ->withSuccess('Vous vous êtes déconnecté avec succès!');;
+    } 
+
+
+
+
+
+
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(RegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/aurhenticate', 'authenticate')->name('authenticate');
+    Route::get('/dashboard', 'dashboard')->name('dashboard');
+    Route::post('/logout', 'logout')->name('logout');
+
+
+
 });
